c5f6115e1dc482b048dfeadb32647793741340b3
Brendo Faisca <bmlfaisca@gmail.com>
2024-05-01 21:17:25 -0300

    Começo das implementações

---
+++ src/Leilao.API/Repositories/LeilaoDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Leilao.API.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Leilao.API.Repositories;
+
+public class LeilaoDbContext : DbContext
+{
+    public DbSet<Auction> Auctions { get; set; }
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlite("Data Source=C:\\Users\\brendo faisca\\source\\repos\\Leilao\\LeilaoDb.db");
+    }
+
+}
